0b0815886e2308d0c082909dcd97c91713a4ad28
kazzzz <kazzz.bobo@gmail.com>
2012-09-23 08:06:08 +0900

    anything

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,6 +38,7 @@
 ;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
 (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
 
 ;;; make Groovy mode electric by default.
@@ -69,3 +70,50 @@
     (turn-on-eldoc-mode)))
 (add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
 
+;; auto-install の設定
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  ;; (setq url-proxy-services '(("http" . "localhost:3128")))
+  (auto-install-compatibility-setup))
+
+;; redo+ の設定
+(when (require 'redo+ nil t)
+  ;; C-' にリドゥを割り当てる
+  (global-set-key (kbd "C-,") 'redo)
+) 
+
+;; anything
+(when (require 'anything nil t)
+  (setq
+   anything-idle-delay 0.3
+   anything-input-idle-delay 0.2
+   anything-candidate-number-limit 100
+   anything-quick-update t
+   anything-enable-shortcuts 'alphabet)
+
+(when (require 'anything-config nil t)
+  (setq anything-su-or-sudo "sudo"))
+
+(require 'anything-match-plugin nil t)
+
+(when (and (executable-find "cmigemo")
+	   (require 'migemo nil t))
+  (require 'anything-migemo nil t))
+
+(when (require 'anything-complete nil t)
+  (anything-lisp-complete-symbol-set-timer 150))
+
+(require 'anything-show-completion nil t)
+
+(when (require 'auto-install nil t)
+  (require 'anything-auto-install nil t))
+
+(when (require 'descbinds-anything nil t)
+  (descbinds-anything-install)))
+
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories
+	       "~/.emacs.d/elisp/ac-dict")
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (ac-config-default))
